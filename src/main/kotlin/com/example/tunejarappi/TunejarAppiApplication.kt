ef63e9bf38a69adfead3702ad25b1da161d26048
NellyKaykay <elelyonexclusive@gmail.com>
2023-05-11 11:53:08 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/tunejarappi/TunejarAppiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tunejarappi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TunejarAppiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TunejarAppiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/tunejarappi/TunejarAppiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tunejarappi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TunejarAppiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
