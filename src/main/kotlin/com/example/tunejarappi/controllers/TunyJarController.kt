825e90e8e0d9807fd15752c4ab150c51050d0def
ikramadam <126071398+ikramadam@users.noreply.github.com>
2023-05-12 12:03:36 +0200

    controllers

--- src/main/kotlin/com/example/tunejarappi/controllers/TunyJarController.kt
+++ src/main/kotlin/com/example/tunejarappi/controllers/TunyJarController.kt
@@ -1,3 +1,15 @@
-package com.example.tunejarappi.controllers
+package com.songs.backend.controllers
 
-data class TunyJarController()
+import com.songs.backend.domain.Song
+import com.songs.backend.repositories.SongRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class SongController(private val songRepository: SongRepository) {
+    @GetMapping("/api/songs")
+    @ResponseBody
+    fun allSongs(): List<Song> = songRepository.findAll()
+}
