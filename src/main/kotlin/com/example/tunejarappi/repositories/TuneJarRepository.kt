5cdca3f63dabf3032bb4c952119280aa34f11423
ikramadam <126071398+ikramadam@users.noreply.github.com>
2023-05-12 12:28:01 +0200

    agrerar basse ce datos

--- src/main/kotlin/com/example/tunejarappi/repositories/TuneJarRepository.kt
+++ src/main/kotlin/com/example/tunejarappi/repositories/TuneJarRepository.kt
@@ -1,4 +1,8 @@
-package com.example.tunejarappi.repositories
+package com.songs.backend.repositories
 
-interface TuneJarRepository {
-}
+import com.songs.backend.domain.Song
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface SongRepository : JpaRepository<Song, Long>
